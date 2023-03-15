93311d81325ba2ca40092cfd65d0e2546c4b0f7a
Ken Williams <33055092+GW3TMH@users.noreply.github.com>
2023-03-15 08:31:50 +0000

    Add files via upload

--- prosakart/__init__.py
+++ prosakart/__init__.py
@@ -1,17 +1,6 @@
-"""
-Tests Georgian vocabulary.
-"""
-
-# Import builtins.
-
-# Import local files.
-from . sql_handle import SQLHandler
-from . interfaces import MainWidget
+#
+# Dummy file to just report version
+#
 
 __version__ = '0.1.0.2'
 
-# Runs the application.
-handler: SQLHandler = SQLHandler()
-widget: MainWidget = MainWidget(handler)
-widget.top.mainloop()
-handler.close()
